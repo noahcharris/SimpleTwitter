bc004dc8ac9a3974970a76fe88fba82987066d75
Noah Harris <noah.christopher.harris@gmail.com>
2014-02-19 01:39:47 -0800

    Working on getTweets method.

--- SimpleTwitter/SimpleTwitterViewController.m
+++ SimpleTwitter/SimpleTwitterViewController.m
@@ -19,6 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     [self.sendButton addTarget:self action:@selector(sendButtonClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self getTweets];
 }
 
 - (void)didReceiveMemoryWarning
@@ -33,6 +34,70 @@
     NSLog(@"hi!!!");
     NSLog(self.textField.text);
 }
+
+
+
+
+- (void)getTweets {
+    ACAccountStore *account = [[ACAccountStore alloc] init];
+    ACAccountType *accountType = [account
+                                  accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+    
+    [account requestAccessToAccountsWithType:accountType
+                                     options:nil completion:^(BOOL granted, NSError *error)
+     {
+         if (granted == YES)
+         {
+             NSArray *arrayOfAccounts = [account
+                                         accountsWithAccountType:accountType];
+             
+             if ([arrayOfAccounts count] > 0)
+             {
+                 ACAccount *twitterAccount = [arrayOfAccounts lastObject];
+                 
+                 NSURL *requestURL = [NSURL URLWithString:@"http://api.twitter.com/1/statuses/home_timeline"];
+                 
+                 NSMutableDictionary *parameters =
+                 [[NSMutableDictionary alloc] init];
+                 [parameters setObject:@"20" forKey:@"count"];
+                 [parameters setObject:@"1" forKey:@"include_entities"];
+                 
+                 SLRequest *postRequest = [SLRequest
+                                           requestForServiceType:SLServiceTypeTwitter
+                                           requestMethod:SLRequestMethodGET
+                                           URL:requestURL parameters:parameters];
+                 
+                 [postRequest setAccount:twitterAccount];
+                 
+                 [postRequest performRequestWithHandler:
+                  ^(NSData *responseData, NSHTTPURLResponse
+                    *urlResponse, NSError *error)
+                  {
+                      self.dataSource = [NSJSONSerialization
+                                         JSONObjectWithData:responseData
+                                         options:NSJSONReadingMutableLeaves
+                                         error:&error];
+                      
+                      
+                      NSLog(@"%d", self.dataSource.count);
+                      NSLog(@"%@", error);
+                      
+                      
+                      if (self.dataSource.count != 0) {
+                          dispatch_async(dispatch_get_main_queue(), ^{
+                              [self.tweetTable reloadData];
+                          });
+                      }
+                  }];
+             }
+         } else {
+             // Handle failure to get account access
+             NSLog(@"Error receiveing tweets...");
+         }
+     }];
+}
+
+
 
 
 #pragma mark TableViewDelegate
